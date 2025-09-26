8c65e5e82e0b3e560e1be2a7d9096a0c71a24e10
mteusgsouza <mteus.g.souza@gmail.com>
2025-09-26 00:39:21 -0300

    formatparams eslint error fix

--- src/utils/FormatParams.tsx
+++ src/utils/FormatParams.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 export default function FormatParams(params: any) {
   const _params = new URLSearchParams(params)
   return _params.toString()
